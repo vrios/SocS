cdf95d504caf5209dad6acba09a2e1da342cc4f7
Vitor Rios <vrios81@gmail.com>
2015-04-13 15:50:51 -0300

    simulations are done 13/04/2015  15:50:51,36

--- done.h
+++ done.h
@@ -1,3 +1,4 @@
 simulations are done 10/04/2015  14:44:48,88 
 simulations are done 10/04/2015  14:53:56,71 
 simulations are done 13/04/2015  15:48:50,92 
+simulations are done 13/04/2015  15:50:51,09 
