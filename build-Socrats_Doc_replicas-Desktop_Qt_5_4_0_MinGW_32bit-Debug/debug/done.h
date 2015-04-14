14bfc90b1634e4a83781f9685039b4af62146cf8
Vitor Rios <vrios81@gmail.com>
2015-04-14 08:52:09 -0300

    simulations are done 14/04/2015   8:52:09,67

--- build-Socrats_Doc_replicas-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/done.h
+++ build-Socrats_Doc_replicas-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/done.h
@@ -1,3 +1,4 @@
 simulations are done 10/04/2015  14:52:57,67 
 simulations are done 14/04/2015   8:41:14,37 
 simulations are done 14/04/2015   8:51:54,34 
+simulations are done 14/04/2015   8:52:09,42 
