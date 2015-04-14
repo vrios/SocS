d3111b79ec85f17dd87a5da3ef7a7f747bcbb78d
Vitor Rios <vrios81@gmail.com>
2015-04-14 08:41:15 -0300

    simulations are done 14/04/2015   8:41:14,71

--- build-Socrats_Doc_replicas-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/done.h
+++ build-Socrats_Doc_replicas-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/done.h
@@ -1,1 +1,2 @@
 simulations are done 10/04/2015  14:52:57,67 
+simulations are done 14/04/2015   8:41:14,37 
--- commit.bat
+++ commit.bat
@@ -1,4 +1,4 @@
 echo simulations are done %DATE%  %TIME% >> done.h
-git add --all
-git commit -a -m "simulations are done %DATE%  %TIME%"
+git add done.h
+git commit -m "simulations are done %DATE%  %TIME%"
 git push origin master
